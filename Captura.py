8482569dfdfc0417de87d0863d38c0a3d2593381
AlexanderNT24 <89727568+AlexanderNT24@users.noreply.github.com>
2021-10-01 02:42:29 -0500

    V8

--- Captura.py
+++ Captura.py
@@ -1,38 +1,32 @@
 import cv2 #Opencv
 import pafy 
 
-class Captura:
+class Captura():
+    tipoEntrada = 'webcam'
+    data = ''
+    url = ''
+    type = "mp4"
+    captura= None
 
-    def __init__(self):   
-        pass
+    def __init__(self, tipoEntrada, url):   
+        self.tipoEntrada = tipoEntrada
+        self.url = url
 
-    def setTipoEntrada(self,tipoEntrada):
-        self.tipoEntrada=tipoEntrada
 
-    def getTipoEntrada(self):
-        return self.tipoEntrada
-
-    def setCaptura(self,captura):
-        self.captura=captura
-
-    def getCaptura(self):
-        if self.getTipoEntrada()=="webcam":        
+    def getData(self):
+        if self.tipoEntrada=="webcam":        
             #Capturo el streaming de la webcam de el puerto de la webcam (0 para la nativa y 1,2.. para externas)
             self.captura=cv2.VideoCapture(0)
-            return self.captura
-        elif self.getTipoEntrada()=="streaming": 
+        elif self.tipoEntrada=="streaming": 
             #Se usa para hacer video streaming de una url de youtube
-            #Hardcodear 
-            url="https://www.youtube.com/watch?v=j6eGHROLKP8&t=17s&ab_channel=RingaTech"
             #Consigue metadatos de un video de youtube (url, vistas, usuario etc)
-            video=pafy.new(url)
-            datosVideo=video.getbest(preftype="mp4")
+            video=pafy.new(self.url)
+            datosVideo=video.getbest(preftype=self.type)
             #Iniciamos captura
             self.captura =cv2.VideoCapture()
             #Abrimos la ubicacion del video
             self.captura.open(datosVideo.url)
             #print(datosVideo.url)
-            return self.captura   
         else:
-            print("NO VALIDA")
-
+            self.captura = None
+        return self.captura
